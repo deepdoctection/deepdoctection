c167e78bd14d1c58be250fa89df3fd00eceab700
Janis <jm@drjanismeyer.de>
2022-01-17 09:07:41 +0100

    optimize imports [dataflow]

--- deep_doctection/utils/__init__.py
+++ deep_doctection/utils/__init__.py
@@ -24,14 +24,15 @@ __all__ = []
 
 
 @no_type_check
-def _global_import(name, prefix: Optional[Union[str,Tuple[str, ...]]] = None,
-                   suffix: Optional[Union[str,Tuple[str, ...]]] = None):
+def _global_import(
+    name, prefix: Optional[Union[str, Tuple[str, ...]]] = None, suffix: Optional[Union[str, Tuple[str, ...]]] = None
+):
     prefix_default = prefix is None
     suffix_default = suffix is None
     p = __import__(name, globals(), None, level=1)  # pylint: disable=C0103
-    lst = p.__all__ if '__all__' in dir(p) else dir(p)
+    lst = p.__all__ if "__all__" in dir(p) else dir(p)
     for k in lst:
-        if not k.startswith('__'):
+        if not k.startswith("__"):
             if prefix_default and suffix_default:
                 globals()[k] = p.__dict__[k]
                 __all__.append(k)
@@ -45,10 +46,10 @@ def _global_import(name, prefix: Optional[Union[str,Tuple[str, ...]]] = None,
                     __all__.append(k)
 
 
-_global_import("file_utils",suffix=("_available","_requirement"))
-_global_import("metacfg",prefix=("set_config_by_yaml","save_config_to_yaml","config_to_cli_str"))
-_global_import("utils", prefix = ("delete_keys_from_dict","split_string","string_to_dict"))
-_global_import("settings",prefix="names")
+_global_import("file_utils", suffix=("_available", "_requirement"))
+_global_import("metacfg", prefix=("set_config_by_yaml", "save_config_to_yaml", "config_to_cli_str"))
+_global_import("utils", prefix=("delete_keys_from_dict", "split_string", "string_to_dict"))
+_global_import("settings", prefix="names")
 
 # pylint: disable=undefined-variable
 __all__.extend(fs.__all__)  # type: ignore
