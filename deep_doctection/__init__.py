91b84cd19a8f4c177f4bb15a81f37dafc8ffc129
Janis <jm@drjanismeyer.de>
2022-01-20 17:27:18 +0100

    optimize imports [analyzer]

--- deep_doctection/__init__.py
+++ deep_doctection/__init__.py
@@ -15,3 +15,10 @@ from .mapper import *  # pylint: disable = W0622
 from .pipe import *
 from .train import *
 from .utils import *
+
+from .utils.file_utils import tf_available, pytorch_available
+from .utils.logger import logger
+
+if not tf_available() and not pytorch_available():
+    logger.info("Neither Tensorflow or Pytorch are available. You will not be able to use any Deep Learning model from"
+                "the library.")
