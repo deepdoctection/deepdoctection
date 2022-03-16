13d4ababe3e4dd2c178e13e3b77fab3292bdf565
Janis <jm@drjanismeyer.de>
2022-03-16 11:05:32 +0100

    optimize matching rules and service [pipe]

--- deepdoctection/mapper/match.py
+++ deepdoctection/mapper/match.py
@@ -39,9 +39,12 @@ def match_anns_by_intersection(
     use_weighted_intersections: bool = False,
     parent_ann_ids: Optional[Union[List[str], str]] = None,
     child_ann_ids: Optional[Union[str, List[str]]] = None,
+    max_parent_only: bool = False
 ) -> Tuple[Any, Any, List[ImageAnnotation], List[ImageAnnotation]]:
     """
-    Generates an iou/ioa-matrix for parent_ann_categories and child_ann_categories.
+    Generates an iou/ioa-matrix for parent_ann_categories and child_ann_categories and returns pairs of child/parent
+    indices that are above some intersection threshold. It will also return a list of all pre selected parent and child
+    annotations.
 
     **Example:**
 
@@ -57,7 +60,17 @@ def match_anns_by_intersection(
     |**p_3**|  1.   |  0.4  |
     +-------+-------+-------+
 
-    With ioa_threshold = 0.5 it will return [[2],[0]], [[1],[],[1]], [c_1,c_2], [p_1,p_2,p_3]
+    With ioa_threshold = 0.5 it will return [[2],[0]], [[1],[],[1]], [c_1,c_2], [p_1,p_2,p_3].
+
+    For each child the sum of all ioas with all parents sum up to 1. Hence, the ioa with one parent will in general
+    decrease if one child intersects with more parents. Take two childs one matching two parents with an ioa of 0.5 each
+    while the second matching four parents with an ioa of 0.25 each. In this situation it is difficult to assign
+    children according to a given threshold and one also has to take into account the number of parental intersection for
+    each child. Setting use_weighted_intersections to True will multiply each ioa with the number of intersection making
+    it easier to work with an absolute threshold.
+
+    In some situation you want to assign to each child at most one parent. Setting max_parent_only to True it will
+    select the parent with the highest ioa. Note, there is currently no implementation for iou.
 
     :param dp: image datapoint
     :param parent_ann_category_names: single str or list of category names
@@ -74,6 +87,7 @@ def match_anns_by_intersection(
                            dates which are not in the list.
     :param child_ann_ids: Additional filter condition. If some ids are selected, it will ignore all other children
                           candidates which are not in the list.
+    :param max_parent_only: Will assign to each child at most one parent with maximum ioa
     :return: child indices, parent indices (see Example), list of parent ids and list of children ids.
     """
 
@@ -101,15 +115,30 @@ def match_anns_by_intersection(
     elif matching_rule in ["ioa"] and parent_anns and child_anns:
         ioa_matrix = np.transpose(np_ioa(parent_ann_boxes, child_ann_boxes))  # type: ignore
 
-        def _weighted_ioa_matrix(mat: NDArray[np.float32]) -> NDArray[np.float32]:
-            sum_of_rows = (mat != 0).sum(1)
-            multiplier = np.transpose(sum_of_rows * np.ones((mat.shape[1], mat.shape[0])))
-            return multiplier * mat
+        if max_parent_only:
+            # set all matrix values below threshold to 0
+            ioa_matrix[ioa_matrix < ioa_threshold]=0
+            # add a dummy column to the left. argmax will choose this column if all ioa values of one child are 0.
+            # This index will be ignored in output
+            ioa_matrix = np.hstack([np.zeros((ioa_matrix.shape[0],1)), ioa_matrix])
+            child_index_arg_max = ioa_matrix.argmax(1)
+            child_index = child_index_arg_max.nonzero()[0]
+            child_index_nonzero = child_index_arg_max[child_index]
+            # reduce parent index by one, as all indices have been increased by one because of the dummy column
+            parent_index = child_index_nonzero- \
+                           np.ones(child_index_nonzero.shape[0], dtype=np.int)
+        else:
 
-        if use_weighted_intersections:
-            ioa_matrix = _weighted_ioa_matrix(ioa_matrix)
-        output = ioa_matrix > ioa_threshold
-        child_index, parent_index = output.nonzero()
+            def _weighted_ioa_matrix(mat: NDArray[np.float32]) -> NDArray[np.float32]:
+                sum_of_rows = (mat != 0).sum(1)
+                multiplier = np.transpose(sum_of_rows * np.ones((mat.shape[1], mat.shape[0])))
+                return multiplier * mat
+
+            if use_weighted_intersections:
+                ioa_matrix = _weighted_ioa_matrix(ioa_matrix)
+            output = ioa_matrix > ioa_threshold
+            child_index, parent_index = output.nonzero()
+
     else:
         return [], [], [], []
 
