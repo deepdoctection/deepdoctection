7901268f7a1bae096b5f1e4b18e78078d8e3c019
Janis <jm@drjanismeyer.de>
2022-03-25 13:53:36 +0100

    update docs [docs]

--- deepdoctection/extern/doctrocr.py
+++ deepdoctection/extern/doctrocr.py
@@ -16,8 +16,9 @@
 # limitations under the License.
 
 """
-
+Deepdoctection wrappers for DocTr OCR text line detection and text recognition models
 """
+
 from typing import List, Tuple
 from ..utils.settings import names
 from ..utils.detection_types import Requirement, ImageType
@@ -32,6 +33,12 @@ if doctr_available() and tf_addons_available():
 
 
 def doctr_predict_text_lines(np_img: ImageType, predictor: "DetectionPredictor") -> List[DetectionResult]:
+    """
+    Generating text line DetectionResult based on Doctr DetectionPredictor.
+    :param np_img: Image in np.array.
+    :param predictor: `doctr.models.detection.predictor.DetectionPredictor`
+    :return: A list of text line detection results (without text).
+    """
     raw_output = predictor([np_img])
     detection_results = [DetectionResult(box=box[:4].tolist(),
                                          class_id=1,
@@ -42,6 +49,15 @@ def doctr_predict_text_lines(np_img: ImageType, predictor: "DetectionPredictor")
 
 
 def doctr_predict_text(inputs: List[Tuple[str,ImageType]], predictor: "RecognitionPredictor") -> List[DetectionResult]:
+    """
+    Calls Doctr text recognition model on a batch of numpy arrays (text lines predicted from a text line detector) and
+    returns the recognized text as DetectionResult
+    :param inputs: list of tuples containing the annotation_id of the input image and the numpy array of the cropped
+                   text line
+    :param predictor: `doctr.models.detection.predictor.RecognitionPredictor`
+    :return: A list of DetectionResult containing recognized text.
+    """
+
     uuids, images = list(zip(*inputs))
     raw_output = predictor(list(images))
     detection_results = [DetectionResult(score=output[1], text=output[0],uuid=uuid) for uuid,output in
@@ -50,11 +66,41 @@ def doctr_predict_text(inputs: List[Tuple[str,ImageType]], predictor: "Recogniti
 
 
 class DoctrTextlineDetector(ObjectDetector):
+    """
+    A deepdoctection wrapper of DocTr text line detector. We model text line detection as ObjectDetector
+    and assume to use this detector in a ImageLayoutService. This model currently uses the default implementation
+    DBNet as described in “Real-time Scene Text Detection with Differentiable Binarization”, using a ResNet-50 backbone.
+    and can be used in either Tensorflow or PyTorch.
 
+    Regarding the model we refer to the documentation https://mindee.github.io/doctr/models.html#
+
+    **Example:**
+
+         .. code-block:: python
+
+                 path = "/path/to/image_dir"
+                 det = DoctrTextlineDetector()
+                 layout = ImageLayoutService(det,to_image=True, crop_image=True)
+                 rec = DoctrTextRecognizer()
+                 text = TextExtractionService(rec,extract_from_roi="LINE")
+                 analyzer = DoctectionPipe(pipeline_component_list=[layout,text])
+                 df = analyzer.analyze(path = path)
+
+                 for dp in df:
+                     ...
+
+
+    """
     def __init__(self):
         self.doctr_predictor = detection_predictor(pretrained=True)
 
     def predict(self, np_img: ImageType) -> List[DetectionResult]:
+        """
+        Prediction per image.
+
+        :param np_img: image as numpy array
+        :return: A list of DetectionResult
+        """
         detection_results = doctr_predict_text_lines(np_img,self.doctr_predictor)
         return detection_results
 
@@ -67,11 +113,42 @@ class DoctrTextlineDetector(ObjectDetector):
 
 
 class DoctrTextRecognizer(TextRecognizer):
+    """
+    A deepdoctection wrapper of DocTr text recognition predictor. The base class is a TextRecognizer that takes
+    a batch of sub images (e.g. text lines from a text detector) and returns a list with text spotted in the sub images.
+    This model currently uses the default implementation CRNN with a VGG-16 backbone as described in
+    “An End-to-End Trainable Neural Network for Image-based Sequence Recognition and Its Application to Scene
+    Text Recognition” and can be used in either Tensorflow or PyTorch.
+
+    Regarding the model we refer to the documentation https://mindee.github.io/doctr/models.html#
+
+    **Example:**
+
+         .. code-block:: python
+
+                 path = "/path/to/image_dir"
+                 det = DoctrTextlineDetector()
+                 layout = ImageLayoutService(det,to_image=True, crop_image=True)
+                 rec = DoctrTextRecognizer()
+                 text = TextExtractionService(rec,extract_from_roi="LINE")
+                 analyzer = DoctectionPipe(pipeline_component_list=[layout,text])
+                 df = analyzer.analyze(path = path)
+
+                 for dp in df:
+                     ...
+
+    """
 
     def __init__(self):
         self.doctr_predictor = recognition_predictor(pretrained=True)
 
     def predict(self, images: List[Tuple[str,ImageType]]) -> List[DetectionResult]:
+        """
+        Prediction on a batch of text lines
+
+        :param images: list of tuples with the annotation_id of the sub image and a numpy array
+        :return: A list of DetectionResult
+        """
         return doctr_predict_text(images, self.doctr_predictor)
 
     @classmethod
