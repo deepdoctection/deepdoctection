ad0ff953dcb6e75f2171d89ec1bcf54b83e631d8
Janis <jm@drjanismeyer.de>
2022-04-23 15:44:23 +0200

    remove location from pr input [mapper]

--- deepdoctection/mapper/prodigystruct.py
+++ deepdoctection/mapper/prodigystruct.py
@@ -182,5 +182,4 @@ def image_to_prodigy(dp: Image) -> JsonDict:
         spans.append(box)
     output["spans"] = spans
     output["meta"] = {"file_name": dp.file_name}
-    output["location"] = dp.location
     return output
