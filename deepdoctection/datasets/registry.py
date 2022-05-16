369593a4794f8c64f2a551ce30bb19780b6943f9
Janis <jm@drjanismeyer.de>
2022-05-16 08:53:15 +0200

    update tests registry [datasets]

--- deepdoctection/datasets/registry.py
+++ deepdoctection/datasets/registry.py
@@ -28,57 +28,7 @@ from termcolor import colored
 from deepdoctection.datasets import instances
 from deepdoctection.datasets.base import DatasetBase
 
-__all__ = ["DatasetRegistry", "dataset_registry"]
-
-
-_DATASETS: Dict[str, Type[DatasetBase]] = dict(
-    (m[1]._name, m[1])  # pylint: disable=W0212
-    for m in inspect.getmembers(instances, inspect.isclass)
-    if issubclass(m[1], DatasetBase) and m[0] != "DatasetBase"
-)
-
-
-class DatasetRegistry:
-    """
-    The DatasetRegistry is the object for receiving datasets and registering new ones. Use the registry to
-    easily generate instances of a dataset.
-    """
-
-    @staticmethod
-    def print_dataset_names() -> None:
-        """
-        Print a list of registered dataset names
-        """
-        print(list(_DATASETS.keys()))
-
-    @staticmethod
-    def get_dataset(name: str) -> DatasetBase:
-        """
-        Returns an instance of a dataset with a given name.
-
-        :param name: A dataset name
-        :return: An instance of a dataset
-        """
-        return _DATASETS[name]()
-
-    @staticmethod
-    def register_dataset(name: str, dataset_class: Type[DatasetBase]) -> None:
-        """
-        Register a new dataset.
-
-        :param name: A dataset name
-        :param dataset_class: A new dataset class to add to the registry.
-        """
-        _DATASETS[name] = dataset_class
-
-    @staticmethod
-    def get_dataset_names() -> List[str]:
-        """
-        Get a list of available dataset names. This will not print the names.
-
-        :return: A list of dataset names.
-        """
-        return list(_DATASETS.keys())
+__all__ = ["dataset_registry"]
 
 
 dataset_registry = catalogue.create("deepdoctection", "datasets", entry_points=True)
