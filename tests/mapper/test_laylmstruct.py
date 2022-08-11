868d1ddbd5924822feb45507c05b85b3118494c2
Janis <jm@drjanismeyer.de>
2022-08-11 09:34:39 +0200

    add tests [tests]

--- tests/mapper/test_laylmstruct.py
+++ tests/mapper/test_laylmstruct.py
@@ -27,10 +27,16 @@ from unittest.mock import MagicMock, patch
 import numpy as np
 from pytest import mark
 
-from deepdoctection.mapper.laylmstruct import image_to_layoutlm, image_to_raw_layoutlm_features
+from deepdoctection.mapper.laylmstruct import image_to_layoutlm, image_to_raw_layoutlm_features, \
+    raw_features_to_layoutlm_features
 from deepdoctection.mapper.xfundstruct import xfund_to_image
 from deepdoctection.utils.detection_types import JsonDict
 from deepdoctection.utils.settings import names
+from deepdoctection.utils.file_utils import transformers_available
+from deepdoctection.datapoint import Image
+
+if transformers_available():
+    from transformers import LayoutLMTokenizerFast
 
 
 @mark.requires_pt
@@ -75,7 +81,7 @@ def test_image_to_layoutlm(
 def test_image_to_raw_layoutlm_features_for_token_data(datapoint_xfund: JsonDict,
                                                        xfund_category_names: Dict[str, str],
                                                        xfund_categories_dict_name_as_key: Dict[str,str],
-                                                       raw_layoutlm_features: JsonDict):
+                                                       raw_layoutlm_features: JsonDict) -> None:
     """
     testing image_to_raw_layoutlm_features is mapping correctly for dataset type "TOKEN_CLASSIFICATION"
     """
@@ -100,7 +106,7 @@ def test_image_to_raw_layoutlm_features_for_token_data(datapoint_xfund: JsonDict
 @patch("deepdoctection.mapper.xfundstruct.load_image_from_file", MagicMock(return_value=np.ones((1000, 1000, 3))))
 def test_image_to_raw_layoutlm_features_for_inference(datapoint_xfund: JsonDict,
                                                        xfund_category_names: Dict[str, str],
-                                                       raw_layoutlm_features: JsonDict):
+                                                       raw_layoutlm_features: JsonDict) -> None:
     """
     testing image_to_raw_layoutlm_features is mapping correctly. Semantic entities and tags have been removed, so that
     the scenario covers the inference case
@@ -127,3 +133,27 @@ def test_image_to_raw_layoutlm_features_for_inference(datapoint_xfund: JsonDict,
     assert "labels" not in raw_features
 
 
+@mark.requires_pt
+def test_raw_features_to_layoutlm_features(dp_image_with_layout_and_word_annotations: Image,
+                                           layoutlm_features: JsonDict) -> None:
+    """
+    testing image_to_layoutlm_features is mapping correctly
+    """
+
+    # Arrange
+    dp = image_to_raw_layoutlm_features()(dp_image_with_layout_and_word_annotations)
+    tokenizer_fast = LayoutLMTokenizerFast.from_pretrained("microsoft/layoutlm-base-uncased")
+
+    # Act
+    feature = raw_features_to_layoutlm_features(dp, tokenizer_fast, padding="do_not_pad")
+
+    # Assert
+    assert feature["image_ids"] == layoutlm_features["image_ids"]
+    assert feature["width"] == layoutlm_features["width"]
+    assert feature["height"] == layoutlm_features["height"]
+    assert feature["ann_ids"] == layoutlm_features["ann_ids"]
+    assert feature["input_ids"] == layoutlm_features["input_ids"]
+    assert feature["token_type_ids"] == layoutlm_features["token_type_ids"]
+    assert feature["attention_mask"] == layoutlm_features["attention_mask"]
+    assert feature["bbox"] == layoutlm_features["bbox"]
+    assert feature["tokens"] == layoutlm_features["tokens"]
