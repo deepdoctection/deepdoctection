03908c7ec41626bb4433d72b92a53708e1f58315
Janis <jm@drjanismeyer.de>
2022-08-15 14:52:08 +0200

    add doclaynet tests [tests]

--- tests/datasets/test_registry.py
+++ tests/datasets/test_registry.py
@@ -31,7 +31,7 @@ def test_dataset_registry_has_all_build_in_datasets_registered() -> None:
     """
     test dataset registry has all pipeline components registered
     """
-    assert len(dataset_registry.get_all()) == 9
+    assert len(dataset_registry.get_all()) == 11
 
 
 def test_dataset_registry_registered_new_dataset() -> None:
