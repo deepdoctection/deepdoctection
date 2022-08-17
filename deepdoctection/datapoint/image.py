835f6227792389df1901e6d8ee617d30573d0cde
Janis <jm@drjanismeyer.de>
2022-08-17 16:24:53 +0200

    add environment optional env vars [extern]

--- deepdoctection/datapoint/image.py
+++ deepdoctection/datapoint/image.py
@@ -512,7 +512,8 @@ class Image:
             image_ann = ImageAnnotation.from_dict(**ann_dict)
             if "image" in ann_dict:
                 image_dict = ann_dict["image"]
-                image_ann.image = cls.from_dict(**image_dict)
+                if image_dict:
+                    image_ann.image = cls.from_dict(**image_dict)
             image.dump(image_ann)
         if summary_dict := kwargs.get("summary"):
             image.summary = SummaryAnnotation.from_dict(**summary_dict)
