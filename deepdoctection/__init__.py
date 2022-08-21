6d4fec1a1f8e92bb715f2004e5cfcd700102bd93
JaMe76 <jm@drjanismeyer.de>
2022-08-21 23:09:51 +0200

    update version [init]

--- deepdoctection/__init__.py
+++ deepdoctection/__init__.py
@@ -12,7 +12,7 @@ from packaging import version
 from .utils.file_utils import get_tf_version, pytorch_available, tf_available
 from .utils.logger import logger
 
-__version__ = 0.15
+__version__ = 0.16
 
 if not tf_available() and not pytorch_available():
     logger.info(
