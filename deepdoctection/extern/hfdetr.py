90a2d59e89dfee2efd7bb698d4f2adc2bbca1ee1
Janis <jm@drjanismeyer.de>
2023-05-07 17:38:31 +0200

    update detr config and setup [extern][force ci]

--- deepdoctection/extern/hfdetr.py
+++ deepdoctection/extern/hfdetr.py
@@ -144,6 +144,7 @@ class HFDetrDerivedDetector(ObjectDetector):
         self.path_weights = path_weights
         self.path_feature_extractor_config = path_feature_extractor_config_json
         self.config = PretrainedConfig.from_pretrained(pretrained_model_name_or_path=self.path_config)
+        self.config.use_timm_backbone = True
         self.config.threshold = 0.1
         self.config.nms_threshold = 0.05
         self.hf_detr_predictor = self.set_model(path_weights)
