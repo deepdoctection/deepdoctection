5c59a2cbc79011f496acb81efc80bd26f965f90b
JaMe76 <jm@drjanismeyer.de>
2023-11-22 16:35:24 +0100

    small fixes [mapper][force ci]

--- setup.py
+++ setup.py
@@ -227,7 +227,7 @@ setup(
     long_description_content_type="text/markdown",
     install_requires=dist_deps,
     extras_require=EXTRA_DEPS,
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests", "tests.*", "tests_d2"]),
     package_data={
         "deepdoctection.configs": ["*.yaml"],
         "deepdoctection.datasets.instances.xsl": ["*.xsl"],
