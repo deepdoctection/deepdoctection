4622226ded22576424c12d38d63cab0cf545ff44
JaMe76 <jm@drjanismeyer.de>
2024-11-25 11:43:51 +0100

    release v.0.36.0

--- deepdoctection/__init__.py
+++ deepdoctection/__init__.py
@@ -24,7 +24,7 @@ from .utils.logger import LoggingRecord, logger
 
 # pylint: enable=wrong-import-position
 
-__version__ = 0.35
+__version__ = 0.36
 
 _IMPORT_STRUCTURE = {
     "analyzer": ["config_sanity_checks", "get_dd_analyzer", "ServiceFactory"],
