ff522d8d9ddd428f9eed89f6e30791fd24fda7b4
JaMe76 <jm@drjanismeyer.de>
2025-12-03 21:48:04 +0100

    add pipe test cases

--- packages/deepdoctection/tests/pipe/conftest.py
+++ packages/deepdoctection/tests/pipe/conftest.py
@@ -15,16 +15,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+import os
 import numpy as np
 
-from pytest import fixture
+import pytest
 
 from dd_core.datapoint.image import Image
+import shared_test_utils as stu
 
-@fixture(name="dp_image")
+@pytest.fixture(name="dp_image")
 def fixture_dp_image() -> Image:
     """fixture Image datapoint"""
     img = Image(location="/test/to/path", file_name="test_name")
     img.image = np.ones([400, 600, 3], dtype=np.float32)
     return img
+
+
+@pytest.fixture
+def pdf_path():
+    # `stu.asset_path('pdf_file_two_pages')` points to a pdf file
+    return stu.asset_path("pdf_file_two_pages")
+
+
+@pytest.fixture
+def image_dir_and_file():
+    # `stu.asset_path('sample_image')` points to a png file; we need its directory for dir test
+    img_path = stu.asset_path("sample_image")
+    img_dir = os.path.dirname(img_path)
+    return img_dir, img_path
+
+
+@pytest.fixture
+def image_bytes(image_dir_and_file):
+    # Build bytes for the single-image test
+    _, img_path = image_dir_and_file
+    with open(img_path, "rb") as f:
+        return f.read()
+
+
+@pytest.fixture
+def identity_pipe():
+    # No pipeline components
+    return DoctectionPipe(pipeline_component_list=[])
