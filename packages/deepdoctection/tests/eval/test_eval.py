97ece8ee6770639db50245982e61a13dd05d3dd3
JaMe76 <jm@drjanismeyer.de>
2026-02-16 21:18:16 +0100

    fix test issues [tests][force ci]

--- packages/deepdoctection/tests/eval/test_eval.py
+++ packages/deepdoctection/tests/eval/test_eval.py
@@ -36,6 +36,7 @@ from dd_core.utils import DatasetType, get_type
 from deepdoctection.eval import CocoMetric, Evaluator
 from deepdoctection.extern.base import DetectionResult
 from deepdoctection.extern.hfdetr import HFDetrDerivedDetector
+from deepdoctection.extern.model import ModelCatalog
 from deepdoctection.pipe.layout import ImageLayoutService
 
 try:
@@ -110,11 +111,14 @@ class TestEvaluator:
         self._dataset.dataflow.build = MagicMock(return_value=DataFromList([dp_image]))
         self._dataset.dataflow.categories = categories
         self._dataset.dataset_info.type = DatasetType.OBJECT_DETECTION
-
+        path_config = ModelCatalog.get_full_path_configs("Aryn/deformable-detr-DocLayNet/model.safetensors")
+        path_weights = ModelCatalog.get_full_path_weights("Aryn/deformable-detr-DocLayNet/model.safetensors")
+        preprocessor_config = ModelCatalog.get_full_path_preprocessor_configs("Aryn/deformable-detr-"
+                                                                              "DocLayNet/model.safetensors")
         self._layout_detector = HFDetrDerivedDetector(
-            path_config_json="",
-            path_weights="",
-            path_feature_extractor_config_json="",
+            path_config_json=path_config,
+            path_weights=path_weights,
+            path_feature_extractor_config_json=preprocessor_config,
             categories=detr_categories,
             device="cpu",
         )
